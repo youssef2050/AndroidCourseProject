887d49176f0fb6517c8cc291831ccc80f028f3b2
Youssef Ezz-Eldeen <yousf2050@hotmail.com>
2021-04-08 17:25:46 +0300

    last edit for coding

--- app/src/main/java/com/menu/androidcourseproject/ui/details/DetailsFragment.java
+++ app/src/main/java/com/menu/androidcourseproject/ui/details/DetailsFragment.java
@@ -106,7 +106,7 @@ public class DetailsFragment extends Fragment {
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(requireActivity(), callback);
     }
-
+//
     private String getDate() {
         return Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "/" + (Calendar.getInstance().get(Calendar.MONTH) + 1) + "/" + Calendar.getInstance().get(Calendar.YEAR) + "-" + Calendar.getInstance().get(Calendar.HOUR_OF_DAY) + ":" + Calendar.getInstance().get(Calendar.MINUTE);
     }
